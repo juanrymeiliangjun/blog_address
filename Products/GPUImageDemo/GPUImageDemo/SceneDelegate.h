a8dff8d9b2af935c34ca8411e1c48d56b001b2c6
meiliangjun1_vendor <meiliangjun1_vendor@sensetime.com>
2021-04-02 18:20:45 +0800

    添加GPUImage工程

---
+++ Products/GPUImageDemo/GPUImageDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  GPUImageDemo
+//
+//  Created by meiliangjun1_vendor on 2021/4/1.
+//  Copyright © 2021 meiliangjun1_vendor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Products/GPUImageDemo/GPUImageDemo/ViewController.m
@@ -0,0 +1,44 @@
+//
+//  ViewController.m
+//  GPUImageDemo
+//
+//  Created by meiliangjun1_vendor on 2021/4/1.
+//  Copyright © 2021 meiliangjun1_vendor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import <GPUImage/GPUImageFramework.h>
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *orginImage;
+@property (weak, nonatomic) IBOutlet UIImageView *filterImageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    UIImage *personImage = [UIImage imageNamed:@"person"];
+    
+    self.orginImage.image = personImage;
+    
+    GPUImageSketchFilter *skFilter = [[GPUImageSketchFilter alloc] init];
+    [skFilter forceProcessingAtSize:personImage.size];
+    [skFilter useNextFrameForImageCapture];
+    
+    GPUImagePicture *picture = [[GPUImagePicture alloc] initWithImage:personImage];
+    [picture addTarget:skFilter];
+    [picture processImage];
+    
+    UIImage *newImage = [skFilter imageFromCurrentFramebuffer];
+    self.filterImageView.image = newImage;
+    
+}
+
+
+@end
