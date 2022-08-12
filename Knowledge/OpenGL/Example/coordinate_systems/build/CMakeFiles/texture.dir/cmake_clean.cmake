71d83937ba502b8680c75ef9dfafad55eb77ace6
meiliangjun1_vendor <meiliangjun1_vendor@sensetime.com>
2022-08-12 19:54:14 +0800

    【mod】添加对OpenGL的学习

---
+++ Knowledge/OpenGL/Example/coordinate_systems/build/CMakeFiles/texture.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/texture.dir/Shader.cpp.o"
+  "CMakeFiles/texture.dir/main.cpp.o"
+  "texture"
+  "texture.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/texture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
